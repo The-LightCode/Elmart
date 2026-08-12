8d601adf35bb273a6c6d7b657e620052b874c613
The-LightCode <orugboedison6@gmail.com>
2026-08-12 20:48:14 +0100

    Update build.sh

--- Elmarrt/backend/build.sh
+++ Elmarrt/backend/build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
-# Your existing build commands (like collecting static files)
+# 1. Install all dependencies from your text file
+pip install -r requirements.txt
+
+# 2. Upgrade the installer and collect the layout files
 python -m pip install --upgrade pip
-python manage.py collectstatic --no-input
+python Elmarrt/backend/manage.py collectstatic --no-input
